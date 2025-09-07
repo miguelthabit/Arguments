9ac44ead16497fdd01ad63dd209d5e039bbadaa7
miguelthabit <miguelndilahomba@gmail.com>
2025-09-07 19:39:54 +0300

    arguments

---
+++ brr.py
@@ -0,0 +1,8 @@
+print("We are going to learn,about functions,my name is MIGUEL,ALD I LIVE IN TANZANIA.")
+def Miguel(name,location,age,hobbies):
+    return f"{name} {location} {age} {hobbies}"
+
+Myresponds= Miguel("Miguel", " my location is Tanzania","My age is 10","My hobby is playing football" )
+print(Myresponds)
+Myresponds2= Miguel("daniel", " my location kenya","My age is 24","My hobby is teaching pupils online" )
+print(Myresponds2)
